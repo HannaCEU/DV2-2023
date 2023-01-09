f6b03c46b0fe2d114a7d46158a1ea42570a36ef2
asipovich_hanna <grazhyna@yahoo.com>
2023-01-09 15:02:49 +0000

    Commit first DV2 R1

---
+++ DV2 R1.R
@@ -0,0 +1,88 @@
+source('http://bit.ly/CEU-R-shoes')
+ls()
+str(students)
+plot(students$shoe, students$math)
+abline(lm(math~shoe, students), col = 'red')
+
+library(GGally)
+
+plot(students)
+
+ggpairs(students)
+
+rm(list=ls())
+?ls
+
+ls(all.names = TRUE)
+
+## visualisation for distance cities in Hungary
+
+download.file('https://bit.ly/hun-cities-distance', 'cities.xls')
+
+library(readxl)
+
+cities <- read_excel('cities.xls')
+
+##get rid of the last row and the first column
+
+cities <- cities [, -1]
+cities <- cities [-nrow(cities), ]
+
+plot(cities)
+
+library(GGally)
+ggpairs(cities)
+
+#building function in R
+mds <- cmdscale(as.dist(cities))
+mds
+plot(mds)
+
+text(mds[, 1], mds[, 2], names (cities))
+
+mds[, 1] <- -1 * mds [, 1]
+plot(mds)
+text(mds[, 1], mds[, 2], names(cities))
+
+## to do redo with ggplot
+library(ggplot2)
+str(mds)
+mds <- data.frame(mds)
+str(mds)
+mds$city <- names(cities)
+str(mds)
+ggplot(mds, aes (X1, X2, label = city)) +
+  geom_text()
+
+library(ggrepel)
+ggplot(mds, aes(X1, X2, label = city)) + geom_text_repel()
+
+# use inbuilt dataset ?eurodist
+mds1 <- cmdscale(eurodist)
+mds1
+mds1 <- data.frame(mds1)
+
+mds1$city <- row.names(mds1)
+
+ggplot(mds1, aes (X1, -X2, label = city)) +
+  geom_text_repel()
+
+
+## inbuilt mtcars 
+?mtcars
+
+mds2 <- cmdscale(dist(mtcars))
+mds2
+mds2 <- data.frame(mds2)
+mds2$car <- row.names(mtcars)
+
+
+ggplot(mds2, aes (X1, X2, label = car)) +
+  geom_text_repel()
+
+
+
+
+
+
+
